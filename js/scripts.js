a6b8c558b86a855ae6f8bce079f62f6ff808c7be
Beverly Potts <pottsbeverly@gmail.com>
2020-06-15 16:19:42 -0400

    Add PlacesIveBeen methods to add Location objects and assign Ids

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 function PlacesIveBeen() {
   this.locations = [];
-  this.currentID = 0;
+  this.currentId = 0;
 }
+
+PlacesIveBeen.prototype.addLocation = function(location) {
+  location.id=this.assignId();
+  this.locations.push(location);
+}
+
+PlacesIveBeen.prototype.assignId = function() {
+  this.currentId++;
+  return this.currentId;
+}
+
 
 function Locations(name, landmarks, seasonVisited, notes) {
   this.name = name;
